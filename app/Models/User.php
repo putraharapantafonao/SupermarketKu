84a71dab542a30bd16679387b155be056bf507fc
Putra Harapan Tafonao <putraharapantafonao199@gmail.com>
2026-07-16 01:01:28 +0700

    Fix MassAssignmentException using stable conveltional fillable property on User Model

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,19 +4,37 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Database\Factories\UserFactory;
-use Illuminate\Database\Eloquent\Attributes\Fillable;
-use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-// 💡 PENTING: Memastikan properti mass assignment mengizinkan role_id, name, email, dan password
-#[Fillable(['name', 'email', 'password', 'role_id'])]
-#[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
+
+    /**
+     * 💡 PERBAIKAN: Menggunakan properti fillable konvensional
+     * untuk memastikan role_id dan data lainnya lolos mass assignment.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'role_id',
+    ];
+
+    /**
+     * 💡 PERBAIKAN: Menggunakan properti hidden konvensional untuk menyembunyikan token.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
 
     /**
      * Get the attributes that should be cast.
